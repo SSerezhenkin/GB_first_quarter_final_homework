ca79e542f1f99a759dd5cbc932d2cc4cce228b56
Сергей Серёженькин <sv.serezhenkin@gmail.com>
2023-02-27 22:35:07 +0300

    finish

--- Program.cs
+++ Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] inputStrings = new string[0];
+
+// запрашиваем у пользователя ввод строк
+while (true)
+{
+    Console.Write("Введите строку (или нажмите Enter для завершения ввода): ");
+    string input = Console.ReadLine();
+
+    if (input == "")
+    {
+        break;
+    }
+
+    // добавляем новую строку в массив
+    Array.Resize(ref inputStrings, inputStrings.Length + 1);
+    inputStrings[inputStrings.Length - 1] = input;
+}
+
+// вызываем метод для фильтрации массива
+string[] filteredStrings = FilterStrings(inputStrings);
+
+// выводим отфильтрованный массив в консоль
+Console.WriteLine("Отфильтрованные строки:");
+foreach (string str in filteredStrings)
+{
+    Console.WriteLine(str);
+}
+
+static string[] FilterStrings(string[] strings)
+{
+    // создаем новый массив для отфильтрованных строк
+    string[] filteredStrings = new string[0];
+
+    // проходим по каждой строке в массиве
+    foreach (string str in strings)
+    {
+        // проверяем длину строки
+        if (str.Length <= 3 && str.Length > 0)
+        {
+            // добавляем отфильтрованную строку в массив
+            Array.Resize(ref filteredStrings, filteredStrings.Length + 1);
+            filteredStrings[filteredStrings.Length - 1] = str;
+        }
+    }
+
+    return filteredStrings;
+}
